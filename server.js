84dd257bd2d80ba3030cd49ff44ea8251a8a67a5
Michael Wang <michaelwang0712@gmail.com>
2015-01-12 21:15:26 -0600

    Able to resize images in server

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var express = require('express'),
 	formidable = require('formidable'),		// used to handle file uploads
 	fs = require('fs'),
 	path = require('path'),
-	util = require('util'),
-	gm = require('gm');
+	util = require('util');
+var im = require('imagemagick');
 
 var app = express();
 
@@ -22,22 +22,29 @@ app.get('/', function(req, res) {
 
 app.post('/upload', function(req, res) {
 	var form = new formidable.IncomingForm();
-	form.uploadDir = __dirname + '/public/uploads';
+	form.uploadDir = __dirname + '/public/uploads/';
 	form.keepExtensions = true;
 	form.multiples = false;
 
 	form.parse(req, function(err, fields, file) {
 		res.writeHead(200, {'content-type': 'text/plain'});
-
-
-
-
-
-
 		res.write('received upload:\n\n');
-		res.end(util.inspect({fields: fields, files: file}));
-	});
+		res.end(util.inspect({fields: fields, file: file}));
 
+		path = file.upload.path;
+		im.resize({
+			srcPath: path,
+			dstPath: "output.jpg",
+			height: 220,
+			width: 238
+		}, function(err, stdout, stderr) {
+			if (!err) {
+				console.log('Resized from ' + path + " to output.jpg");
+			} else {
+				console.log(err);
+			}
+		});
+	});
 });
 
 app.listen(8080);
